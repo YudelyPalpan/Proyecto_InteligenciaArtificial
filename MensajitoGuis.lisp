15f136297149e0a99d75e3af4920ae72c79f3831
luis.chavez <luchor13@gmail.com>
2017-09-25 00:19:18 -0500

    Primera regla

--- MensajitoGuis.lisp
+++ MensajitoGuis.lisp
@@ -1,3 +1,40 @@
+; tablero
+(setq tablero
+      '(
+       (1 0 1 0 1 0 1 0)
+       (0 0 0 0 0 0 0 0)
+       (0 0 0 0 0 0 0 0)
+       (0 0 0 0 0 0 0 0)
+       (0 0 0 0 0 0 0 0)
+       (0 0 0 0 0 0 0 0)
+       (0 0 0 0 0 0 0 0)
+       (0 0 0 9 0 0 0 0)
+      )
+)
+
+(defun avanzar-gato-derecha (estado)
+  
+  ;(format t "~&Tablero: ~S" (nth 0 estado))
+  ;(format t "~&Posicion: ~S" (nth 1 estado))
+  ;(format t "~&Turno: ~S" (nth 2 estado))
+  (let 
+    (
+     (tab (nth 0 estado)) 
+     (turno (nth 2 estado))
+     (i (nth 0 (nth 1 estado)))
+     (j (nth 1 (nth 1 estado)))
+    )
+    (cond
+      (
+       (and (or (< i 7)  (< j 7)) (= turno 1) (= (nth (+ j 1) (nth (+ i 1) tab))))
+       (setq (nth (+ j 1) (nth (+ i 1) tab)) 1)
+       (setq (nth j (nth i tab)) 0)
+       (return (tab (list (+ i 1) (+ j 1)) 9))
+      )
+     )
+   )
+)
+
 (defun mensaje (mensaje)
  print "holi amotito *-*"
  print "ati?"
